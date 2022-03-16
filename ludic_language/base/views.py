697481c32917d41852b9c12420831e25373534b4
Gabrielle Azadian <gabrielleazadian@gmail.com>
2022-03-16 08:29:39 +0100

    css and legal_notice, about us template done

--- ludic_language/base/views.py
+++ ludic_language/base/views.py
@@ -3,3 +3,11 @@ from django.views.generic import TemplateView
 
 class BaseView(TemplateView):
     template_name = "index.html"
+
+
+class LegalNoticeView(TemplateView):
+    template_name = "legal_notice.html"
+
+
+class AboutUsView(TemplateView):
+    template_name = "about_us.html"
