8f8a6c3c0b87033336e17f6a9ec7b93e916031a8
Gabrielle Azadian <gabrielleazadian@gmail.com>
2022-03-18 10:18:03 +0100

    wsgi file changed

--- ludic_language/wsgi.py
+++ ludic_language/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
+from dotenv import load_dotenv, find_dotenv
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ludic_language.settings')
+load_dotenv(find_dotenv())
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ludic_language.settings.prod')
 
 application = get_wsgi_application()
