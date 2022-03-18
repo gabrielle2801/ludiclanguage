3d3ff3a6304a95cd8ebd134e0b103992ebf486bb
Gabrielle Azadian <gabrielleazadian@gmail.com>
2022-03-18 14:03:58 +0100

    prod --> static_root

--- ludic_language/settings/prod.py
+++ ludic_language/settings/prod.py
@@ -32,7 +32,7 @@ sentry_sdk.init(
     send_default_pii=True
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Activate Django-Heroku.
 django_heroku.settings(locals())
