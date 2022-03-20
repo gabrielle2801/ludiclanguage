0d0e01b088df49d829d9e7b374a53c5ab8916797
Gabrielle Azadian <gabrielleazadian@gmail.com>
2022-03-20 19:22:33 +0100

    add S3

--- ludic_language/settings/prod.py
+++ ludic_language/settings/prod.py
@@ -31,11 +31,7 @@ DATABASES = {
         'PORT': '5432'
     }
 }
-
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
+# Amazon S3
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3ManifestStaticStorage'
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
@@ -43,6 +39,11 @@ AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_S3_REGION_NAME = 'eu-west-2'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = 'static/'
+
+
 # STATICFILES_STORAGE = 'ludic_language.storage.S3Storage'
 # Activate Django-Heroku.
 django_heroku.settings(locals())
