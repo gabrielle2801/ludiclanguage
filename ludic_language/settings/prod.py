258235c127951ad7fbcd04366d18993dd68e6505
Gabrielle Azadian <gabrielleazadian@gmail.com>
2022-03-24 11:12:18 +0100

    AWS_URL delete

--- ludic_language/settings/prod.py
+++ ludic_language/settings/prod.py
@@ -36,12 +36,12 @@ DATABASES = {
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
-AWS_URL = os.getenv("AWS_URL")
-STATIC_URL = AWS_URL + '/static/'
+# AWS_URL = os.getenv("AWS_URL")
+# STATIC_URL = AWS_URL + '/static/'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 MEDIA_URL = AWS_URL + '/media/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-AWS_S3_REGION_NAME = 'eu-west-2'
+# AWS_S3_REGION_NAME = 'eu-west-2'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # STATICFILES_STORAGE = 'ludic_language.storage.S3Storage'
