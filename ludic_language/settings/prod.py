e0a2a6b2a4bee18fe581febf63cc5c9ce92e3b41
Gabrielle Azadian <gabrielleazadian@gmail.com>
2022-03-24 19:11:16 +0100

    changed prod

--- ludic_language/settings/prod.py
+++ ludic_language/settings/prod.py
@@ -31,7 +31,12 @@ DATABASES = {
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME")
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_S3_REGION_NAME = os.getenv("AWS_S3_REGION_NAME")
+AWS_S3_SIGNATURE_VERSION = os.getenv("AWS_S3_SIGNATURE_VERSION")
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # AWS_S3_REGION_NAME = 'eu-west-2'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
