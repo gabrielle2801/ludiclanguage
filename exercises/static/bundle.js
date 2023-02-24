d1255ec50f47d650ac2bff985d9e226dfd7b83b8
Gabrielle Azadian <gabrielleazadian@gmail.com>
2023-02-24 14:21:47 +0100

    webpack work in progress

--- exercises/static/bundle.js
+++ exercises/static/bundle.js
@@ -7,8 +7,26 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./frontend/src/index.js":
+/*!*******************************!*\
+  !*** ./frontend/src/index.js ***!
+  \*******************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError [ERR_MODULE_NOT_FOUND]: Cannot find package 'babel-plugin-syntax-dynamic-import' imported from /Users/xavgab/Worksplace/13_Projet 13/language_project/babel-virtual-resolve-base.js\\n    at new NodeError (/Users/xavgab/Worksplace/13_Projet 13/language_project/node_modules/@babel/core/lib/vendor/import-meta-resolve.js:2240:5)\\n    at packageResolve (/Users/xavgab/Worksplace/13_Projet 13/language_project/node_modules/@babel/core/lib/vendor/import-meta-resolve.js:2776:9)\\n    at moduleResolve (/Users/xavgab/Worksplace/13_Projet 13/language_project/node_modules/@babel/core/lib/vendor/import-meta-resolve.js:2804:18)\\n    at defaultResolve (/Users/xavgab/Worksplace/13_Projet 13/language_project/node_modules/@babel/core/lib/vendor/import-meta-resolve.js:2835:13)\\n    at /Users/xavgab/Worksplace/13_Projet 13/language_project/node_modules/@babel/core/lib/vendor/import-meta-resolve.js:2855:14\\n    at Generator.next (<anonymous>)\\n    at asyncGeneratorStep (/Users/xavgab/Worksplace/13_Projet 13/language_project/node_modules/@babel/core/lib/vendor/import-meta-resolve.js:45:103)\\n    at _next (/Users/xavgab/Worksplace/13_Projet 13/language_project/node_modules/@babel/core/lib/vendor/import-meta-resolve.js:46:194)\\n    at /Users/xavgab/Worksplace/13_Projet 13/language_project/node_modules/@babel/core/lib/vendor/import-meta-resolve.js:46:364\\n    at new Promise (<anonymous>)\");\n\n//# sourceURL=webpack://language_project/./frontend/src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
 /******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./frontend/src/index.js"]();
 /******/ 	
 /******/ })()
 ;
