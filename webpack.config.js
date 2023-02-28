a20fe1b5633a583f55a6ae7f44876a769902adcd
Gabrielle Azadian <gabrielleazadian@gmail.com>
2023-02-28 10:34:51 +0100

    refresh code work in progress React

--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,21 @@ const path = require('path')
 module.exports = {
     entry: './frontend/src/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: 'game.js',
         path: path.resolve(__dirname, 'exercises', 'static')
     },
     module: {
-        rules: [
-            {
-              test: /\.jsx?$/, // compilation to es6
-              exclude: /node_modules/,
-              use: {
-                loader: 'babel-loader',
-                options: {
-                  presets: ['react', 'es2015', 'es2016', 'stage-0'],
-                  plugins: ['syntax-dynamic-import'],
-                },
-              },
-            },
+      rules: [
+        {
+        test: /\.js|.jsx$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+        },
+        {
+          test: /\.css$/,
+        exclude: /node_modules/,
+        use: ["style-loader", "css-loader"],
+        }
       ]
     }
 }
