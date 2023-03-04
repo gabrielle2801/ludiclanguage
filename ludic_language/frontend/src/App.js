358b63551180a89e3b3735b7aa3d05ac4000c7a3
Gabrielle Azadian <gabrielleazadian@gmail.com>
2023-03-04 12:32:27 +0100

    memory-game ok

--- ludic_language/frontend/src/App.js
+++ ludic_language/frontend/src/App.js
@@ -1,17 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './App.css';
-import loup from "../assets/img/Loup.jpg";
-import renard from "../assets/img/Renard.jpg";
+import loup from "../assets/img/Loup.png";
+import renard from "../assets/img/Renard.png";
+import SingleCard from "./components/SingleCard";
 
 
 const cardImages = [
-    { "src": loup },
-    { "src": renard },
+    { "src": loup, matched: false },
+    { "src": renard, matched: false },
 ]
 
 function App() {
     const [cards, setCards] = useState([])
     const [turns, setTurns] = useState(0)
+    const [choiceOne, setChoiceOne] = useState(null)
+    const [choiceTwo, setChoiceTwo] = useState(null)
+    const [disabled, setDisabled] = useState(false)
 
     // shuffle cards
     const shuffleCards = () => {
@@ -19,26 +23,63 @@ function App() {
             .sort(() => Math.random() - 0.5)
             .map((card) => ({ ...card, id: Math.random()}))
 
+        setChoiceOne(null)
+        setChoiceTwo(null)
         setCards(shuffleCards)
         setTurns(0)
     }
-console.log(cards, turns)
+// handle a choice
+const handleChoice =(card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+
+}
+// compare 2 selected cards
+useEffect (() => {
+    if (choiceOne && choiceTwo) {
+        setDisabled(true)
+        if (choiceOne.src === choiceTwo.src) {
+            setCards(prevCards => {
+                return prevCards.map(card => {
+                    if (card.src === choiceOne.src){
+                        return {...card, matched: true}
+                    } else {
+                        return card
+                    }
+                })
+            })
+            resetTurn()
+        } else {
+            setTimeout(() => resetTurn(), 1000)
+        }
+    }
+}, [choiceOne, choiceTwo])
+
+console.log(cards)
+// reset choices @ increase turn 
+const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+    setDisabled(false)
+}
+useEffect(()=> {
+    shuffleCards()
+}, [])
     return (
         <div className='App'>
             <button onClick={shuffleCards}>New Game</button>
-            <div className= "card-grid">
+            <div className= "memory-game">
                 {cards.map(card => (
-                    <div className ="card" key={card.id}>
-                        <div>
-                            <h1>Test</h1>
-                            <img className ="front" src={card.src} alt="card front" />
-                        </div>
-                    </div>
-                    ))}
-
+                    <SingleCard 
+                    key={card.id} 
+                    card={card}
+                    handleChoice={handleChoice}
+                    flipped = {card === choiceOne || card === choiceTwo || card.matched}
+                    disabled={disabled}
+                    />
+                ))}
             </div>
-
-            
+            <p>Turns: {turns}</p>    
         </div>
     )
 }
---
+++ ludic_language/frontend/src/components/SingleCard.js
@@ -0,0 +1,24 @@
+import './SingleCard.css';
+import React, { Component, PropTypes } from 'react';
+import cover from "/assets/img/cover.png";
+
+export default function SingleCard({card, handleChoice, flipped, disabled }){
+    const handleClick = ()=> {
+        if (!disabled){
+            handleChoice(card)
+        }
+        
+    }
+    return(
+        <div className ="memory-card">
+            <div className={flipped ? "flipped": ""}>
+                <img className ="front" src={card.src} alt="card front" />
+                <img 
+                    className ="back" 
+                    src={cover} 
+                    onClick={handleClick}
+                    alt="card back" />
+            </div>
+        </div>
+    )
+}
